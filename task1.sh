d0907c32d12342d1d181f14a40ec62add67f25dd
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 17:32:53 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -19,7 +19,7 @@ while IFS=, read -r id location name role email
 do
     formatted_name=$(echo "$name" | awk '{print toupper(substr($0, 1, 1)) tolower(substr($0, 2))}')
 
-    formatted_email="${formatted_name,,}@abc.com"
+        formatted_email=$(echo "$formatted_name" | tr ' ' '.' | tr '[:lower:]' '[:lower:]')@abc.com
 
     echo "$id,$location,$formatted_name,$role,$formatted_email" >> "$output_file"
 done < "$input_file"
