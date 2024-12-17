c9a4b69c12dcb06c0278e09192d1d312c6366a04
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 22:19:10 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -31,6 +31,7 @@ generate_email() {
 > "$output_file"
 
 declare -A name_count
+declare -A email_map
 
 temp_file=$(mktemp)
 awk -F, '{
@@ -46,6 +47,7 @@ while IFS=, read -r id location name title email department; do
 
   full_name="${name,,}"
   name_count["$full_name"]=$((name_count["$full_name"] + 1))
+  email_map["$email"]="$full_name"
 done < "$temp_file"
 
 while IFS=, read -r id location name title email department; do
@@ -64,10 +66,10 @@ while IFS=, read -r id location name title email department; do
     final_email=$(generate_email "$first_name" "$surname" "$location" "$count")
 
     # Check if the email is a duplicate
-    if [ "$count" -gt 1 ]; then
-      # Add location_id to email if it's a duplicate
-      echo "${final_email,,}${location_id}@abc.com"
-    fi
+    if [ "${email_map[$final_email]}" ]; then
+        # Add location_id to email if it's a duplicate
+        final_email="${final_email,,}${location}@abc.com"
+      fi
 
     echo "$id,$location,$formatted_name,$title,$final_email,$department" >> "$output_file"
 done < "$temp_file"
