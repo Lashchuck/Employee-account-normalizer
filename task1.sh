d3cf943f48da8e74d9c57a1303e36cec6317d76e
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 18:05:48 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 path_to_accounts_csv"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 accounts.csv"
     exit 1
 fi
 
-input_file="$1"
+input_file=$1
 output_file="accounts_new.csv"
 
-if [ ! -f "$input_file" ]; then
-    echo "File not found!"
-    exit 1
-fi
+format_name_email() {
+    local name="$1"
+    local email="$2"
+    local location_id="$3"
 
-> "$output_file"
+    formatted_name=$(echo "$name" | awk '{ for (i=1; i<=NF; i++) printf "%s%s", toupper(substr($i, 1, 1)), tolower(substr($i, 2)) }')
 
-while IFS=, read -r id location name role email
-do
-    formatted_name=$(echo "$name" | awk -F' ' '{ for(i=1;i<NF;i++) printf "%s ", toupper(substr($i,1,1)) tolower(substr($i,2)); printf "%s", toupper(substr($NF,1,1)) tolower(substr($NF,2)) }')
+    formatted_email=$(echo "$formatted_name" | awk '{ print tolower(substr($1, 1)) tolower($2) "@" "abc.com" }')
 
-    first_name_initial=$(echo "$name" | awk '{print tolower(substr($1, 1, 1))}')
-    last_name=$(echo "$name" | awk '{for(i=2;i<=NF;i++) printf "%s ", $i; printf "%s", $NF}')
-    formatted_email="${formatted_name,,}@abc.com"
+    echo "$formatted_name,$location_id,$formatted_email"
+}
 
-    echo "$id,$location,$formatted_name,$role,$formatted_email" >> "$output_file"
+while IFS=, read -r id location_id name department email; do
+
+    formatted_data=$(format_name_email "$name" "$email" "$location_id")
+
+    echo "$id,$formatted_data" >> "$output_file"
 done < "$input_file"
 
-echo "File '$output_file' created successfully."
+echo "Updated accounts saved to $output_file"
