550efe89408281fc70f3d58b1e7f7848f28b3abe
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 18:54:04 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -12,23 +12,27 @@ format_name() {
   echo "$1" | awk -F, '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1'
 }
 
-generate_email(){
+generate_email() {
   local name="$1"
   local surname="$2"
   local location_id="$3"
   local count="$4"
+
+  # Generuj bazowy email bez location_id
   local formatted_name="${name:0:1}${surname,,}"
 
-    if [ "$count" -gt 1 ]; then
-        echo "${formatted_name,,}${location_id}@abc.com"
-      else
-        echo "${formatted_name,,}@abc.com"
-      fi
+  # Dodaj location_id tylko jeśli dana osoba pojawia się więcej niż raz
+  if [ "$count" -gt 1 ]; then
+    echo "${formatted_name,,}${location_id}@abc.com"
+  else
+    echo "${formatted_name,,}@abc.com"
+  fi
 }
 
 # Create or clear the output file
 > "$output_file"
 
+# Zlicz wystąpienia każdego imienia i nazwiska
 declare -A name_count
 while IFS=, read -r id location name title email department; do
   if [[ "$id" == "id" ]]; then
@@ -39,21 +43,21 @@ while IFS=, read -r id location name title email department; do
   name_count["$full_name"]=$((name_count["$full_name"] + 1))
 done < "$input_file"
 
-while IFS=, read -r id location name title email department
-do
+# Przetwarzaj dane i generuj wyniki
+while IFS=, read -r id location name title email department; do
   if [[ "$id" == "id" ]]; then
+    echo "id,location,name,title,email,department" >> "$output_file"
     continue
   fi
 
-  # Extract first name and surname
   first_name="${name%% *}"
   surname="${name##* }"
+
+  formatted_name=$(format_name "$first_name" "$surname")
+  full_name="${name,,}"
   count=${name_count["$full_name"]}
 
-  formatted_name=$(format_name "$first_name,$surname")
   email=$(generate_email "$first_name" "$surname" "$location" "$count")
 
   echo "$id,$location,$formatted_name,$title,$email,$department" >> "$output_file"
 done < "$input_file"
-
-echo "The script has finished processing. The accounts_new.csv file has been created."
