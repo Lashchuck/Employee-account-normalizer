d5b098b241e96d452e480c5b850d5500ee749672
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 19:42:26 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+
 if [ -z "$1" ]; then
-  echo "Usage: $0 path/to/accounts.csv"
+  echo "Usage: $0 patch/to/accounts.csv"
   exit 1
 fi
 
@@ -9,64 +10,54 @@ input_file="$1"
 output_file="accounts_new.csv"
 
 format_name() {
-  local first_name="$1"
-  local surname="$2"
-  echo "${first_name^} ${surname^}"
+  echo "$1" | awk -F, '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1'
 }
 
-generate_email() {
-  local first_name="$1"
+generate_email(){
+  local name="$1"
   local surname="$2"
   local location_id="$3"
   local count="$4"
 
-  local formatted_email="${first_name:0:1}${surname,,}"
-  if [ "$count" -gt 1 ]; then
-    echo "${formatted_email,,}${location_id}@abc.com"
-  else
-    echo "${formatted_email,,}@abc.com"
-  fi
+  local formatted_name="${name:0:1}${surname,,}"
+
+    if [ "$count" -gt 1 ]; then
+        echo "${formatted_name,,}${location_id}@abc.com"
+      else
+        echo "${formatted_name,,}@abc.com"
+      fi
 }
 
+# Create or clear the output file
 > "$output_file"
 
 declare -A name_count
-
-temp_file=$(mktemp)
-awk -F, '{
-  gsub(/^"|"$/, "", $0);
-  gsub(/,+$/, "", $0);
-  print
-}' "$input_file" > "$temp_file"
-
 while IFS=, read -r id location name title email department; do
   if [[ "$id" == "id" ]]; then
     continue
   fi
 
-  full_name="${name,,}"
+  full_name="${name,,}" # Klucz do sprawdzenia liczby wystąpień
   name_count["$full_name"]=$((name_count["$full_name"] + 1))
-done < "$temp_file"
+done < "$input_file"
 
-while IFS=, read -r id location name title email department; do
+while IFS=, read -r id location name title email department
+do
   if [[ "$id" == "id" ]]; then
-    echo "$id,$location,$name,$title,$email,$department" >> "$output_file"
     continue
   fi
 
-  title=$(echo "$title" | sed 's/"//g')
   first_name="${name%% *}"
   surname="${name##* }"
 
-  formatted_name=$(format_name "$first_name" "$surname")
+  formatted_name=$(format_name "$first_name,$surname")
   full_name="${name,,}"
   count=${name_count["$full_name"]}
+
 
   email=$(generate_email "$first_name" "$surname" "$location" "$count")
 
   echo "$id,$location,$formatted_name,$title,$email,$department" >> "$output_file"
-done < "$temp_file"
-
-rm -f "$temp_file"
+done < "$input_file"
 
 echo "The script has finished processing. The accounts_new.csv file has been created."
