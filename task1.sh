8aa93ca9cd77b52b1706c17f3b92db3820a4e895
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 22:30:24 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -21,7 +21,7 @@ generate_email() {
   local count="$4"
 
   local formatted_email="${first_name:0:1}${surname,,}"
-  if [ "$count" -gt 1 ]; then
+  if [ "$count" -gt 2 ]; then
     echo "${formatted_email,,}${location_id}@abc.com"
   else
     echo "${formatted_email,,}@abc.com"
