09a9b3cd1a71dbd5a0ed2072fc47f8319d0a8651
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 22:52:23 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -58,8 +58,8 @@ while IFS=, read -r id location name title email department; do
   surname="${name##* }"
 
   formatted_name="${first_name:0:1}${surname,,}"
-  count=${name_count["$formatted_name"]}
   full_name="${name,,}"
+  count=${name_count["$full_name"]}
 
     final_email=$(generate_email "$first_name" "$surname" "$location" "$count")
 
