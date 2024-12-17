39f393959017a1bdec0d731bd648ec35d40aba78
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 18:08:14 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -10,19 +10,18 @@ output_file="accounts_new.csv"
 
 format_name_email() {
     local name="$1"
-    local email="$2"
-    local location_id="$3"
+    local location_id="$2"
 
     formatted_name=$(echo "$name" | awk '{ for (i=1; i<=NF; i++) printf "%s%s", toupper(substr($i, 1, 1)), tolower(substr($i, 2)) }')
 
-    formatted_email=$(echo "$formatted_name" | awk '{ print tolower(substr($1, 1)) tolower($2) "@" "abc.com" }')
+    formatted_email=$(echo "$formatted_name" | awk '{ print tolower(substr($1, 1)) tolower($2) location_id "@abc.com" }')
 
     echo "$formatted_name,$location_id,$formatted_email"
 }
 
 while IFS=, read -r id location_id name department email; do
 
-    formatted_data=$(format_name_email "$name" "$email" "$location_id")
+    formatted_data=$(format_name_email "$name" "$location_id")
 
     echo "$id,$formatted_data" >> "$output_file"
 done < "$input_file"
