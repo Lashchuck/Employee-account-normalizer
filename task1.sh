a062bf8b848892ca4e4f25acfbf24e79a543d574
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-18 00:14:04 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -28,20 +28,18 @@ generate_email() {
   fi
 }
 
-> "$output_file"
-
 declare -A name_count
 
 temp_file=$(mktemp)
 
-# Poprawne przetwarzanie CSV z uwzględnieniem cudzysłowów i przecinków
+# Parsowanie CSV i zapis do pliku tymczasowego z pełną obsługą cudzysłowów i przecinków
 awk -v OFS=',' '
-  BEGIN { FPAT = "([^,]+)|(\"[^\"]+\")" }
-  NR == 1 { print; next }                  # Przepisanie nagłówka
+  BEGIN { FS=OFS=","; FPAT="([^,]+)|(\"[^\"]+\")" }
+  NR==1 { print; next }                        # Przepisz nagłówek bez zmian
   {
-    gsub(/\r/, "")                         # Usunięcie znaków powrotu karetki
+    gsub(/\r/, "");                            # Usuń znaki powrotu karetki
     for (i = 1; i <= NF; i++) {
-      gsub(/^"|"$/, "", $i)                # Usunięcie cudzysłowów otaczających
+      gsub(/^"|"$/, "", $i)                    # Usuń otaczające cudzysłowy
     }
     print
   }
@@ -50,33 +48,29 @@ awk -v OFS=',' '
 # Pierwsze przejście: zliczanie wystąpień nazw
 while IFS=, read -r id location name title email department; do
   [[ "$id" == "id" ]] && continue
-
   first_name="${name%% *}"
   surname="${name##* }"
   formatted_name="${first_name:0:1}${surname,,}"
-
   name_count["$formatted_name"]=$((name_count["$formatted_name"] + 1))
 done < "$temp_file"
 
 # Drugie przejście: generowanie wyniku
-while IFS=, read -r id location name title email department; do
-  if [[ "$id" == "id" ]]; then
-    printf "%s,%s,%s,%s,%s,%s\n" "$id" "$location" "$name" "$title" "$email" "$department" >> "$output_file"
-    continue
-  fi
+{
+  read -r header
+  printf "%s\n" "$header" > "$output_file"  # Zapis nagłówka
 
-  first_name="${name%% *}"
-  surname="${name##* }"
+  while IFS=, read -r id location name title email department; do
+    first_name="${name%% *}"
+    surname="${name##* }"
+    formatted_name=$(format_name "$first_name" "$surname")
+    count=${name_count["${first_name:0:1}${surname,,}"]}
+    final_email=$(generate_email "$first_name" "$surname" "$location" "$count")
 
-  formatted_name=$(format_name "$first_name" "$surname")
-  count=${name_count["${first_name:0:1}${surname,,}"]}
-
-  final_email=$(generate_email "$first_name" "$surname" "$location" "$count")
-
-  # Właściwy format dla title i pozostałych pól
-  printf "%s,%s,%s,\"%s\",%s,%s\n" \
-    "$id" "$location" "$formatted_name" "$title" "$final_email" "$department" >> "$output_file"
-done < "$temp_file"
+    # Poprawne generowanie linii z obsługą pól zawierających przecinki
+    printf "%s,%s,%s,\"%s\",%s,%s\n" \
+      "$id" "$location" "$formatted_name" "$title" "$final_email" "$department" >> "$output_file"
+  done
+} < "$temp_file"
 
 rm -f "$temp_file"
 
