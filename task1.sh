0fa5d0d3cec92b6a41bf3ba2110f6263088f641e
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 22:19:51 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -66,10 +66,10 @@ while IFS=, read -r id location name title email department; do
     final_email=$(generate_email "$first_name" "$surname" "$location" "$count")
 
     # Check if the email is a duplicate
-    if [ "${email_map[$final_email]}" ]; then
-        # Add location_id to email if it's a duplicate
-        final_email="${final_email,,}${location}@abc.com"
-      fi
+    if [ "$count" -gt 1 ]; then
+      # Add location_id to email if it's a duplicate
+      echo "${final_email,,}${location_id}@abc.com"
+    fi
 
     echo "$id,$location,$formatted_name,$title,$final_email,$department" >> "$output_file"
 done < "$temp_file"
