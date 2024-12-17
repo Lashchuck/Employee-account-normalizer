dceab697028ec8ae1c29b44b844a19e298f0416a
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 17:54:42 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -17,12 +17,11 @@ fi
 
 while IFS=, read -r id location name role email
 do
-    formatted_name=$(echo "$name" | awk '{for(i=1;i<NF;i++) printf "%s ", toupper(substr($i,1,1)) tolower(substr($i,2)); printf "%s", toupper(substr($NF,1,1)) tolower(substr($NF,2))}')
-
-        # Generowanie identyfikatora e-mail
-        first_name_initial=$(echo "$name" | awk '{print tolower(substr($1, 1, 1))}')
+    first_name_initial=$(echo "$name" | awk '{print tolower(substr($1, 1, 1))}')
         last_name=$(echo "$name" | awk '{for(i=2;i<=NF;i++) printf "%s ", $i; printf "%s", $NF}')
-        formatted_email="${first_name_initial}${last_name,,}@abc.com"
+
+        formatted_name=$(echo "$first_name_initial" | awk '{print toupper(substr($0, 1, 1)) tolower(substr($0, 2))}')$(echo "$last_name" | awk '{print tolower($0)}')
+        formatted_email="${formatted_name,,}@abc.com"
 
     echo "$id,$location,$formatted_name,$role,$formatted_email" >> "$output_file"
 done < "$input_file"
