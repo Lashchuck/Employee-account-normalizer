86958ed78b0e04743c358520f979451f81371494
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 17:02:02 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -1,39 +1,27 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 patch/to/accounts.csv"
-  exit 1
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 path_to_accounts_csv"
+    exit 1
 fi
 
 input_file="$1"
 output_file="accounts_new.csv"
 
-format_name() {
-  echo "$1" | awk -F, '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1'
-}
+if [ ! -f "$input_file" ]; then
+    echo "File not found!"
+    exit 1
+fi
 
-generate_email(){
-  local name="$1"
-  local surname="$2"
-  local location_id="$3"
-  echo "${name:0:1}${surname,,}${location_id}@abc.com"
-}
+touch "$output_file"
 
-> "$output_file"
-
-while IFS=, read -r id location name title email department
+while IFS=, read -r id location name role email
 do
-  if [[ "$id" == "id" ]]; then
-    continue
-  fi
+    formatted_name=$(echo "$name" | awk '{print toupper(substr($0, 1, 1)) tolower(substr($0, 2))}')
 
-  first_name="${name%% *}"
-  surname="${name##* }"
+    formatted_email=$(echo "$formatted_name" | tr ' ' | awk '{print tolower($1)}')@abc.com
 
-  formatted_name=$(format_name "$first_name,$surname")
-  email=$(generate_email "$first_name" "$surname" "$location")
-
-  echo "$id,$location,$formatted_name,$title,$email,$department" >> "$output_file"
+    echo "$id,$location,$formatted_name,$role,$formatted_email," >> "$output_file"
 done < "$input_file"
 
-echo "The script has finished processing. The accounts_new.csv file has been created."
+echo "File '$output_file' created successfully."
