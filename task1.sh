6926a8786b18319c5adeb966743c814e2f27297f
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-17 16:12:20 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 path/to/accounts.csv"
-    exit 1
+  echo "Usage: $0 patch/to/accounts.csv"
+  exit 1
 fi
 
 input_file="$1"
 output_file="accounts_new.csv"
 
 format_name() {
-    echo "$1" | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1'
+  echo "$1" | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1'
 }
 
-generate_email() {
-    local name="$1"
-    local surname="$2"
-    echo "${name:0:1}${surname,,}@abc.com"
+generate_email(){
+  local name="$1"
+  local surname="$2"
+  local location_id="$3"
+  echo "${name:0:1}${surname,,}${location_id}@abc.com"
 }
 
-while IFS=, read -r id location name position
+# Skrypty zakładają, że pierwsza linia pliku accounts.csv to nagłówki
+while IFS=, read -r id location name title email department
 do
+  if [[ "$id" == "id" ]]; then
+    continue
+  fi
 
-    if [[ "$id" == "id" ]]; then
-        continue
-    fi
+  first_name="${name%% *}"
+  surname="${name##* }"
 
-    first_name="${name%% *}"
-    surname="${name##* }"
+  formatted_name=$(format_name "$first_name $surname")
+  email=$(generate_email "$first_name" "$surname" "$location")
 
-    formatted_name=$(format_name "$first_name $surname")
-    email=$(generate_email "$first_name" "$surname")
-
-    echo "$id,$location,$formatted_name,$position,$email" >> "$output_file"
+  echo "$id,$location,$formatted_name,$title,$email,$department" >> "$output_file"
 done < "$input_file"
