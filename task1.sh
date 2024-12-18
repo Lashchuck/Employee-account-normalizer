b0a424999d9cff993a9e389fee7cbdd7994da677
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-18 15:24:11 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 if [ -z "$1" ]; then
   printf "Usage: %s path/to/accounts.csv\n" "$0" >&2
   exit 1
@@ -18,46 +19,76 @@ generate_email() {
   local first_name="$1"
   local surname="$2"
   local location_id="$3"
-  local base_email="${first_name:0:1}${surname,,}"
-  printf "%s@abc.com" "${base_email,,}${location_id}"
+  local count="$4"
+
+  local formatted_email="${first_name:0:1}${surname,,}"
+  if [[ "$count" -gt 1 ]]; then
+    printf "%s%s@abc.com" "${formatted_email,,}" "${location_id}"
+  else
+    printf "%s@abc.com" "${formatted_email,,}"
+  fi
 }
 
-declare -A email_count
+declare -A email_count  # Tablica do śledzenia unikalnych e-maili
+declare -A name_count   # Tablica do zliczania nazw
 
-# Tworzenie nowego pliku wynikowego
+temp_file=$(mktemp)
+
+# Parsowanie CSV z obsługą przecinków w polach i cudzysłowów
+awk -v OFS=',' '
+  BEGIN { FS=OFS=","; FPAT="([^,]+)|(\"[^\"]+\")" }
+  NR==1 { print; next }                        # Przepisz nagłówek bez zmian
+  {
+    gsub(/\r/, "");                            # Usuń znaki powrotu karetki
+    print
+  }
+' "$input_file" > "$temp_file"
+
+# Pierwsze przejście: Zliczanie wystąpień nazw
+while IFS=, read -r id location name title email department; do
+  [[ "$id" == "id" ]] && continue
+  first_name="${name%% *}"
+  surname="${name##* }"
+  formatted_name="${first_name:0:1}${surname,,}"
+  name_count["$formatted_name"]=$((name_count["$formatted_name"] + 1))
+done < "$temp_file"
+
+# Drugie przejście: Generowanie wyniku
 {
   read -r header
-  printf "%s\n" "$header"
+  printf "%s\n" "$header" > "$output_file"  # Zapis nagłówka
 
-  while IFS=, read -r id location_id name title email department; do
-    # Obsługa pola `title` zawierającego przecinki
-    while [[ "$title" != *\" && "$department" == "" ]]; do
-      IFS=, read -r next_part
-      title="$title,$next_part"
-    done
+  while IFS=, read -r id location name title email department; do
+    # Obsługa pola `title` z cudzysłowami i przecinkami
+    if [[ "$title" == \"* && "$title" != *\" ]]; then
+      while IFS=, read -r extra; do
+        title="$title,$extra"  # Łączenie fragmentów pola `title`
+        [[ "$title" == *\" ]] && break
+      done
+    fi
 
-    # Formatowanie nazwy
+    # Formatowanie imienia i nazwiska
     first_name="${name%% *}"
     surname="${name##* }"
     formatted_name=$(format_name "$first_name" "$surname")
 
-    # Generowanie e-maila
-    unique_email=$(generate_email "$first_name" "$surname" "$location_id")
+    # Generowanie unikalnego e-maila
+    base_email="${first_name:0:1}${surname,,}"
+    unique_email="${base_email,,}@abc.com"
 
-    # Jeśli e-mail już istnieje, dodaj `location_id` do nazwy użytkownika
-    while [[ -n "${email_count["$unique_email"]}" ]]; do
-      unique_email=$(generate_email "$first_name" "$surname" "${location_id}a")
-    done
+    # Dodanie location_id w przypadku powtórzeń
+    if [[ -n "${email_count["$unique_email"]}" || ${name_count["$base_email"]} -gt 1 ]]; then
+      unique_email="${base_email,,}${location}@abc.com"
+    fi
 
+    # Śledzenie użycia e-maila
     email_count["$unique_email"]=1
 
     # Zapis do pliku wynikowego
     printf "%s,%s,%s,%s,%s,%s\n" \
-      "$id" "$location_id" "$formatted_name" "$title" "$unique_email" "$department"
+      "$id" "$location" "$formatted_name" "$title" "$unique_email" "$department" >> "$output_file"
   done
-} < <(awk -v OFS=',' '
-  BEGIN { FS=OFS=","; FPAT="([^,]+)|(\"[^\"]+\")" }
-  NR==1 { print; next }
-  { gsub(/\r/, ""); print }
-' "$input_file") > "$output_file"
+} < "$temp_file"
+
+rm -f "$temp_file"
 printf "The script has finished processing. The file '%s' has been created.\n" "$output_file"
