9b40facc2cb1d38fe3926efac3463b631594c36e
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-18 01:04:00 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 path/to/accounts.csv"
+  printf "Usage: %s path/to/accounts.csv\n" "$0" >&2
   exit 1
 fi
 
@@ -9,57 +9,70 @@ input_file="$1"
 output_file="accounts_new.csv"
 
 format_name() {
-  echo "$1" | awk -F, '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1'
+  local first_name="$1"
+  local surname="$2"
+  printf "%s %s" "${first_name^}" "${surname^}"
 }
 
-generate_email(){
-  local name="$1"
+generate_email() {
+  local first_name="$1"
   local surname="$2"
   local location_id="$3"
   local count="$4"
 
-  local formatted_name="${name:0:1}${surname,,}"
-
-    if [ "$count" -gt 1 ]; then
-        echo "${formatted_name,,}${location_id}@abc.com"
-      else
-        echo "${formatted_name,,}@abc.com"
-      fi
+  local formatted_email="${first_name:0:1}${surname,,}"
+  if [[ "$count" -gt 1 ]]; then
+    printf "%s%s@abc.com" "${formatted_email,,}" "${location_id}"
+  else
+    printf "%s@abc.com" "${formatted_email,,}"
+  fi
 }
 
-# Create or clear the output file
-> "$output_file"
-
 declare -A name_count
+
+temp_file=$(mktemp)
+
+# Parsowanie CSV i zapis do pliku tymczasowego z pełną obsługą cudzysłowów i przecinków
+awk -v OFS=',' '
+  BEGIN { FS=OFS=","; FPAT="([^,]+)|(\"[^\"]+\")" }
+  NR==1 { print; next }                        # Przepisz nagłówek bez zmian
+  {
+    gsub(/\r/, "");                            # Usuń znaki powrotu karetki
+    for (i = 1; i <= NF; i++) {
+      gsub(/^"|"$/, "", $i)                    # Usuń otaczające cudzysłowy
+      if (i == 4) gsub(/^"|"$/, "", $i)       # Usuń cudzysłowy tylko z 4 kolumny (title)
+    }
+    print
+  }
+' "$input_file" > "$temp_file"
+
+# Pierwsze przejście: zliczanie wystąpień nazw
 while IFS=, read -r id location name title email department; do
-  if [[ "$id" == "id" ]]; then
-    continue
-  fi
-
-  full_name="${name,,}" # Klucz do sprawdzenia liczby wystąpień
-  name_count["$full_name"]=$((name_count["$full_name"] + 1))
-done < "$input_file"
-
-while IFS=, read -r id location name title email department
-do
-  if [[ "$id" == "id" ]]; then
-    continue
-  fi
-
-  # Extract first name and surname
+  [[ "$id" == "id" ]] && continue
   first_name="${name%% *}"
   surname="${name##* }"
+  formatted_name="${first_name:0:1}${surname,,}"
+  name_count["$formatted_name"]=$((name_count["$formatted_name"] + 1))
+done < "$temp_file"
 
-  formatted_name=$(format_name "$first_name,$surname")
-  full_name="${name,,}"
-  count=${name_count["$full_name"]}
+# Drugie przejście: generowanie wyniku
+{
+  read -r header
+  printf "%s\n" "$header" > "$output_file"  # Zapis nagłówka
 
-  # Usuwanie cudzysłowów z kolumny title
-  title=$(echo "$title" | sed 's/^"//;s/"$//')
+  while IFS=, read -r id location name title email department; do
+    first_name="${name%% *}"
+    surname="${name##* }"
+    formatted_name=$(format_name "$first_name" "$surname")
+    count=${name_count["${first_name:0:1}${surname,,}"]}
+    final_email=$(generate_email "$first_name" "$surname" "$location" "$count")
 
-  email=$(generate_email "$first_name" "$surname" "$location" "$count")
+    # Poprawne generowanie linii z obsługą pól zawierających przecinki
+    printf "%s,%s,%s,%s,%s,%s \
+      "$id" "$location" "$formatted_name" "$title" "$final_email" "$department" >> "$output_file"
+  done
+} < "$temp_file"
 
-  echo "$id,$location,$formatted_name,\"$title\",$email,$department" >> "$output_file"
-done < "$input_file"
+rm -f "$temp_file"
 
-echo "The script has finished processing. The accounts_new.csv file has been created."
+printf "The script has finished processing. The file '%s' has been created.\n" "$output_file"
