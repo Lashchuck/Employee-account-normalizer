a13aa95301cf10ea10c4cd3c9f0b71501af3e352
Mateusz Laszczak <mateusz.laszczak19@gmail.com>
2024-12-18 01:25:04 +0100

    task1

--- task1.sh
+++ task1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  printf "Usage: %s path/to/accounts.csv\n" "$0" >&2
+  echo "Usage: $0 path/to/accounts.csv"
   exit 1
 fi
 
@@ -11,7 +11,7 @@ output_file="accounts_new.csv"
 format_name() {
   local first_name="$1"
   local surname="$2"
-  printf "%s %s" "${first_name^}" "${surname^}"
+  echo "${first_name^} ${surname^}"
 }
 
 generate_email() {
@@ -21,57 +21,53 @@ generate_email() {
   local count="$4"
 
   local formatted_email="${first_name:0:1}${surname,,}"
-  if [[ "$count" -gt 1 ]]; then
-    printf "%s%s@abc.com" "${formatted_email,,}" "${location_id}"
+  if [ "$count" -gt 1 ]; then
+    echo "${formatted_email,,}${location_id}@abc.com"
   else
-    printf "%s@abc.com" "${formatted_email,,}"
+    echo "${formatted_email,,}@abc.com"
   fi
 }
+
+> "$output_file"
 
 declare -A name_count
 
 temp_file=$(mktemp)
+awk -F, '{
+  gsub(/^"|"$/, "", $0);
+  gsub(/,+$/, "", $0);
+  print
+}' "$input_file" > "$temp_file"
 
-# Parsowanie CSV i zapis do pliku tymczasowego z pełną obsługą cudzysłowów i przecinków
-awk -v OFS=',' '
-  BEGIN { FS=OFS=","; FPAT="([^,]+)|(\"[^\"]+\")" }
-  NR==1 { print; next }                        # Przepisz nagłówek bez zmian
-  {
-    gsub(/\r/, "");                            # Usuń znaki powrotu karetki
-    for (i = 1; i <= NF; i++) {
-      gsub(/^"|"$/, "", $i)                    # Usuń otaczające cudzysłowy
-    }
-    print
-  }
-' "$input_file" > "$temp_file"
-
-# Pierwsze przejście: zliczanie wystąpień nazw
 while IFS=, read -r id location name title email department; do
-  [[ "$id" == "id" ]] && continue
+  if [[ "$id" == "id" ]]; then
+    continue
+  fi
+
   first_name="${name%% *}"
-  surname="${name##* }"
-  formatted_name="${first_name:0:1}${surname,,}"
-  name_count["$formatted_name"]=$((name_count["$formatted_name"] + 1))
+    surname="${name##* }"
+    formatted_name="${first_name:0:1}${surname,,}"
+
+    name_count["$formatted_name"]=$((name_count["$formatted_name"] + 1))
 done < "$temp_file"
 
-# Drugie przejście: generowanie wyniku
-{
-  read -r header
-  printf "%s\n" "$header" > "$output_file"  # Zapis nagłówka
+while IFS=, read -r id location name title email department; do
+  if [[ "$id" == "id" ]]; then
+    echo "$id,$location,$name,$title,$email,$department" >> "$output_file"
+    continue
+  fi
 
-  while IFS=, read -r id location name title email department; do
-    first_name="${name%% *}"
-    surname="${name##* }"
-    formatted_name=$(format_name "$first_name" "$surname")
-    count=${name_count["${first_name:0:1}${surname,,}"]}
+  first_name="${name%% *}"
+  surname="${name##* }"
+
+  formatted_name="${first_name:0:1}${surname,,}"
+  count=${name_count["$formatted_name"]}
+
     final_email=$(generate_email "$first_name" "$surname" "$location" "$count")
 
-    # Poprawne generowanie linii z obsługą pól zawierających przecinki
-    printf "%s,%s,%s,%s,%s,%s\n" \
-      "$id" "$location" "$formatted_name" "$title" "$final_email" "$department" >> "$output_file"
-  done
-} < "$temp_file"
+    echo "$id,$location,$formatted_name,$title,$final_email,$department" >> "$output_file"
+done < "$temp_file"
 
 rm -f "$temp_file"
 
-printf "The script has finished processing. The file '%s' has been created.\n" "$output_file"
+echo "The script has finished processing. The accounts_new.csv file has been created."
